a5fd32f5f23d39eb8876700609539a356c99fc3d
Nanda-25 <113986489+Nanda-25@users.noreply.github.com>
2023-01-14 12:31:48 +0800

    project set up

---
+++ crowdfunding/projects/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+
+from .models import Project
+
+class ProjectSerializer(serializers.Serializer):
+    id = serializers.ReadOnlyField()
+    title = serializers.CharField()
+    description = serializers.CharField(max_length=200)
+    goal = serializers.IntegerField()
+    image = serializers.URLField()
+    is_open = serializers.BooleanField()
+    date_created = serializers.DateTimeField()
+    owner = serializers.CharField(max_length=200)
+
+    def create(self, validated_data):
+        return Project.objects.create(**validated_data)
